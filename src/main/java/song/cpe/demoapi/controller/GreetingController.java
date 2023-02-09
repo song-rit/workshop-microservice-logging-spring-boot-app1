9c6543831d6de24b35316a24ea0f65fd5726ef1a
song.cpe <song.cpe@gmail.com>
2023-02-10 01:17:43 +0700

    refactor controller to /isOddNumber/{digit}

--- src/main/java/song/cpe/demoapi/controller/GreetingController.java
+++ src/main/java/song/cpe/demoapi/controller/GreetingController.java
@@ -1,16 +1,44 @@
 package song.cpe.demoapi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/greeting")
+@RequestMapping("/api/v1/digit")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("hello");
+    private Logger logger = LoggerFactory.getLogger(GreetingController.class.getName());
+
+    @GetMapping(value = "/isOddNumber/{digit}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResultResponse> isUpperLetter(
+            @PathVariable Integer digit
+            ) {
+
+        logger.info("digit: {}", digit);
+        ResultResponse response = new ResultResponse();
+        response.result = true;
+        return ResponseEntity.ok(response);
+    }
+
+    class ResultResponse{
+
+        boolean result;
+
+        public boolean isResult() {
+            return result;
+        }
+
+        public void setResult(boolean result) {
+            this.result = result;
+        }
     }
 }
+
+
+
